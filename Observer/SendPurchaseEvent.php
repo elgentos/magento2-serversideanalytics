2e85f02ec8ee722d3db33ea8b5b486f5fc132017
Wouter Steenmeijer <wouter@steenmeijer.nu>
2023-09-19 16:34:42 +0200

    Fixed session id in dataobject

--- Observer/SendPurchaseEvent.php
+++ Observer/SendPurchaseEvent.php
@@ -135,7 +135,7 @@ class SendPurchaseEvent implements ObserverInterface
                 'tax' => $invoice->getBaseTaxAmount(),
                 'shipping' => ($this->getPaidShippingCosts($invoice) ?? 0),
                 'coupon_code' => $order->getCouponCode(),
-                'session_id' => $orderExtensionAttributes->getGaSessionId()
+                'session_id' => $order->getExtensionAttributes()->getGaSessionId()
             ]
         );
 
